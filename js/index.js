d983e26c9adc23de60815a5eb3e0c770411addb4
5hye <62659333+5hye@users.noreply.github.com>
2020-12-14 15:57:41 +0900

    Add files via upload

---
+++ js/index.js
@@ -0,0 +1,27 @@
+$(document).ready(function(){
+    
+    $(".make-10").on("click", function() {
+        DrawCircle(); })
+     $(".make-20").on("click", function() {
+        DrawCircle(); })
+    $(".make-30").on("click", function() {
+            DrawCircle(); })
+    $(".make-40").on("click", function() {
+                DrawCircle(); })
+    
+
+})
+
+
+
+function DrawCircle(){
+
+
+    for (var i = 30; i<100; i++) {
+      
+        var windowWidth = $(window).width() * Math.random()
+        var windowHeigtht = $(window).width() * Math.random()
+        $(".circle-container").append("<div class='circle varr' style='left:" + windowWidth + "px; top:"+windowHeigtht+ "px;>'></div>")
+        }
+    
+    }
